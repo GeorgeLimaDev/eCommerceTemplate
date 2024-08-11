e256dcba1b616b8aca3564236d96449ab361f3b6
GeorgeLimaDev <george.lima@vtex.com.br>
2024-08-11 09:53:56 -0300

    WIP: Landing page, catálogo e contato

--- js/itens_cardapio.js
+++ js/itens_cardapio.js
@@ -1,63 +1,63 @@
 const itens = [
   {
-  nome: 'Café',
-  imagem: "../css/Imagens/cafe.jpg",
-  valor: 'R$ 4,00'
+  nome: 'Mini estátua: Sanfoneiro',
+  imagem: "https://static.wixstatic.com/media/11062b_1dcdc0f68a45432c92f66d4a714038b2~mv2.jpg/v1/fill/w_640,h_438,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_1dcdc0f68a45432c92f66d4a714038b2~mv2.jpg",
+  valor: 'R$ 30,00'
   },
   {
-  nome: 'Cappucino',
-  imagem: "../css/Imagens/capuccino.jpg",
-  valor: 'R$ 6,00'
+  nome: 'Mini estátua: Músico',
+  imagem: "https://briselier.com/wp-content/uploads/2021/08/nordeste2.jpeg",
+  valor: 'R$ 40,00'
   },
   {
-  nome: 'Mocha',
-  imagem: "../css/Imagens/mocha.jpg",
-  valor: 'R$ 6,50'
+  nome: 'Conjunto de cerâmicas',
+  imagem: "https://briselier.com/wp-content/uploads/2021/08/centrooeste3.jpg",
+  valor: 'R$ 100,00'
   },
   {
-  nome: 'Latte',
-  imagem: "../css/Imagens/latte.jpg",
-  valor: 'R$ 7,00'
+  nome: 'Mini estátua: Bela Moça',
+  imagem: "https://blog.xiquexiquebrasil.com.br/wp-content/uploads/2019/06/original-546f2c52e986cd7f11f4935c98ba29d3.jpg",
+  valor: 'R$ 35,00'
   },
   {
-  nome: 'Refrigerante',
-  imagem: "../css/Imagens/refrigerante.jpg",
-  valor: 'R$ 5,00'
+  nome: 'Decorativo Ipês',
+  imagem: "https://gmconline.com.br/wp-content/uploads/2022/10/WhatsApp-Image-2021-09-17-at-11.55.02.jpeg",
+  valor: 'R$ 20,00'
   },
   {
-  nome: 'Água',
-  imagem: "../css/Imagens/agua.jpg",
-  valor: 'R$ 3,00'
+  nome: 'Decorativo Casinhas',
+  imagem: "https://tokemcasa.tokstok.com.br/wp-content/uploads/2022/09/artesanato-brasileiro-04.jpg",
+  valor: 'R$ 25,00'
   },
   {
-  nome: 'Suco (300mL)',
-  imagem: "../css/Imagens/suco.jpg",
-  valor: 'R$ 6,00'
+  nome: 'Decorativo Tatu 1:1',
+  imagem: "https://vivaagreste.com.br/wp-content/uploads/2023/06/tatu-de-barro-artesanato-agreste-pernambuco-scaled.jpg",
+  valor: 'R$ 50,00'
   },
   {
-  nome: 'Suco (1L)',
-  imagem: "../css/Imagens/jarra.jpg",
-  valor: 'R$ 12,00'
+  nome: 'Jarros decorados',
+  imagem: "https://imagens-revista-pro.vivadecora.com.br/uploads/2020/12/Artesanato-Brasileiro-vaso-de-cer%C3%A2mica-redondo-com-figuras-tribais-da-artista-Elen-Castro-Cruz-Nascimento-Foto-Artesol.jpg",
+  valor: 'R$ 120,00'
   },
   {
-  nome: 'Empada',
-  imagem: "../css/Imagens/empada.jpg",
-  valor: 'R$ 5,00'
+  nome: 'Conjunto: Animais',
+  imagem: "https://tokemcasa.tokstok.com.br/wp-content/uploads/2022/09/artesanato-brasileiro-03.jpg",
+  valor: 'R$ 50,00'
   },
   {
-  nome: 'Bolo de chocolate (fatia)',
-  imagem: "../css/Imagens/boloo.jpg",
-  valor: 'R$ 7,00'
+  nome: 'Animais de madeira',
+  imagem: "https://agenciadenoticias.ms.gov.br/wp-content/uploads/2023/01/WhatsApp-Image-2023-01-24-at-11.04.14.jpeg",
+  valor: 'R$ 20,00'
   },
   {
-  nome: 'Croassant',
-  imagem: "../css/Imagens/croissant.jpg",
-  valor: 'R$ 6,00'
+  nome: 'Decorativos: Fauna',
+  imagem: "https://www.bonitonoticias.com.br/imagem/noticia/1000/1000/1660766856_62185.jpg",
+  valor: 'R$ 35,00'
   },
   {
-  nome: 'Pão de queijo (8un)',
-  imagem: "../css/Imagens/pao-de-queijo.jpg",
-  valor: 'R$ 12,00'
+  nome: 'Conjunto: Trançados',
+  imagem: "https://static.liderinteriores.com.br/wp-content/uploads/2022/01/3-12.jpg",
+  valor: 'R$ 150,00'
   }
 
 ];
